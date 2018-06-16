65fcebccaca9c12fffbb1a5a3bf7f71b8de8c7c1
markconstable <mark@trevoapp.com>
2018-06-15 20:55:32 -0400

    Bug fixing local storage

--- app.js
+++ app.js
@@ -106,6 +106,26 @@ function removeTask(e) {
     e.target.parentElement.parentElement.remove();
   }
 }
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+}
+
+// Remove from LS
+function removeTaskFromLocalStorage(taskItem) {
+  let tasks;
+  if (localStorage.getItem('tasks') === null){
+    tasks = [];localStorage.setItem('tasks', JSON.stringify(tasks));
+  } else {
+    tasks = JSON.parse(localStorage.getItem('tasks'));
+  }
+
+  tasks.forEach(function(task, index){
+    if(taskItem.textContent === task){
+      tasks.splice(index, 1);
+    }
+  });
+
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+
 }
 
 function clearTasks(){
@@ -115,8 +135,15 @@ function clearTasks(){
   while(taskList.firstChild) {
     taskList.removeChild(taskList.firstChild) // jsperf.com/innerhtml-vs-removechild
   }
+  localStorage.setItem('tasks', JSON.stringify(tasks));
+  // Clear from LS
+  clearTasksFromLocalStorage();
 }
 
+//Clear Tasks from Local Storage function
+function clearTasksFromLocalStorage() {
+  localStorage.removeItem('tasks');
+}
 // Filter Tasks
 function filterTasks(e){
   const text = e.target.value.toLowerCase(); // e is event object and method target is caught from event listener and then value is value of target
